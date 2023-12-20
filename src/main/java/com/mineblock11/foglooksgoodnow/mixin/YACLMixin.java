3ae1b068ebf6f7cf7b62af9557deb7f75fc1ad80
Calum <contact@mineblock11.dev>
2023-12-20 18:06:48 +0000

    fix: YACL

---
+++ src/main/java/com/mineblock11/foglooksgoodnow/mixin/YACLMixin.java
@@ -0,0 +1,27 @@
+package com.mineblock11.foglooksgoodnow.mixin;
+
+import dev.isxander.yacl3.gui.YACLScreen;
+import net.minecraft.client.gui.tab.TabManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(value = YACLScreen.class, remap = false)
+public class YACLMixin {
+    @Shadow @Final public TabManager tabManager;
+
+    @Shadow private boolean pendingChanges;
+
+    @Inject(method = "finishOrSave", at = @At(value = "INVOKE", target = "Ljava/util/Set;forEach(Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER), cancellable = false)
+    public void YACL_FIX(CallbackInfo ci) {
+        pendingChanges = false;
+        if (tabManager.getCurrentTab() instanceof YACLScreen.CategoryTab categoryTab) {
+            categoryTab.updateButtons();
+        }
+    }
+}

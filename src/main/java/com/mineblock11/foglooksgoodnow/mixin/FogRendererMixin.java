0b2e800e30f72b09ca65a0179db0cce16d510d84
Louis <82884007+Dev0Louis@users.noreply.github.com>
2024-03-04 21:31:41 +0100

    Better system for fog manipulation (#38)
    Co-authored-by: D1p4k <82884007+D1p4k@users.noreply.github.com>

--- src/main/java/com/mineblock11/foglooksgoodnow/mixin/FogRendererMixin.java
+++ src/main/java/com/mineblock11/foglooksgoodnow/mixin/FogRendererMixin.java
@@ -1,23 +1,22 @@
 package com.mineblock11.foglooksgoodnow.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mineblock11.foglooksgoodnow.FogManager;
 import com.mineblock11.foglooksgoodnow.config.FLGConfig;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
 import net.minecraft.client.render.FogShape;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(BackgroundRenderer.class)
@@ -41,22 +40,22 @@ public class FogRendererMixin {
             blue = (float) MathHelper.lerp(undergroundFactor, blue, fogColor.z * blue);
         }
     }
-    @Inject(method = "applyFog", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void fogRenderEvent(Camera camera, BackgroundRenderer.FogType fogMode, float viewDistance, boolean thickFog, float partialTick, CallbackInfo ci, CameraSubmersionType fogType, Entity entity, BackgroundRenderer.FogData fogData) {
+    @Inject(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", remap = false, shift = At.Shift.BEFORE))
+    private static void fogRenderEvent(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci, @Local BackgroundRenderer.FogData fogData) {
         if(FLGConfig.get().disableAll) return;
         if (camera.getSubmersionType() == CameraSubmersionType.NONE) {
             FogManager fogManager = FogManager.instance();
 
             float undergroundFogMultiplier = 1.0F;
             if (FogManager.shouldRenderCaveFog()) {
-                undergroundFogMultiplier = (float)  MathHelper.lerp(fogManager.getUndergroundFactor(partialTick), fogManager.caveFogMultiplier, 1.0F);
-                float darkness = fogManager.darkness.get(partialTick);
+                undergroundFogMultiplier = (float)  MathHelper.lerp(fogManager.getUndergroundFactor(tickDelta), fogManager.caveFogMultiplier, 1.0F);
+                float darkness = fogManager.darkness.get(tickDelta);
                 undergroundFogMultiplier = MathHelper.lerp(darkness, undergroundFogMultiplier, 1.0F);
             }
 
-            RenderSystem.setShaderFogStart(viewDistance * fogManager.fogStart.get(partialTick));
-            RenderSystem.setShaderFogEnd(viewDistance * (fogManager.fogEnd.get(partialTick) * fogManager.fogStartRain.get(partialTick)) * undergroundFogMultiplier);
-            RenderSystem.setShaderFogShape(FogShape.SPHERE);
+            fogData.fogStart = viewDistance * fogManager.fogStart.get(tickDelta);
+            fogData.fogEnd = viewDistance * (fogManager.fogEnd.get(tickDelta) * fogManager.fogStartRain.get(tickDelta)) * undergroundFogMultiplier;
+            fogData.fogShape = FogShape.SPHERE;
         }
     }
 }
